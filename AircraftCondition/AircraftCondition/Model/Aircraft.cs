f31d101da4ee464a34710ed70cbfc0499642abf4
Dmitry <106148379+Dmit1@users.noreply.github.com>
2023-11-01 20:50:31 +0300

    Start

---
+++ AircraftCondition/AircraftCondition/Model/Aircraft.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AircraftCondition.AircraftCondition.Model
+{
+    public class Aircraft
+    {
+        int AircraftID {  get; set; }  
+        string Name { get; set; }
+        string RegName { get; set; }
+        string ManufactureDate { get; set; }
+        List<Component> Components { get; set; }
+
+        Aircraft(int aircraftID,string name, string regName, string manufactureDate)
+        {
+            AircraftID = aircraftID;
+            Name = name;
+            RegName = regName;
+            ManufactureDate = manufactureDate;
+            Components = new List<Component>();     
+        }
+    }
+}
