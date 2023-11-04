d41c199895bf8f58709d3f7fd4b32a77eb8b9980
Dmitry <106148379+Dmit1@users.noreply.github.com>
2023-11-04 18:44:43 +0300

    Добавление вормы Планирования

---
+++ AircraftCondition/UIForms/Plan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AircraftCondition.UIForms
+{
+    public partial class Plan : Form
+    {
+        public Plan()
+        {
+            InitializeComponent();
+        }
+    }
+}
